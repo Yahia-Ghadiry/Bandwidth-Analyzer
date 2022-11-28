51a5087817e4f8b3a357f3f993a44fb70c774841
Yahia-Ghadiry <yahiaghadiry@gmail.com>
2022-11-28 18:47:09 +0200

    completed stoarge of requests in a good manar to help in next step: displaying them

--- background/background.js
+++ background/background.js
@@ -1,14 +1,38 @@
-// Format given data to return only needed data and better handling of long 
-const intervals = ["year", "month", "week_day", "day", "hour", "minute"] 
+// intervals of saved data
+const intervals = ["year", "month", "week_day", "day", "hour"] 
+
+
+// recursive function so all usage from a frames of a website will be added to same url not to the frame's
+function findOrginOfFrame(requestDetails)
+{
+    if (requestDetails["frameId"])
+    {
+        return findOrginOfFrame(requestDetails.frameAncestors[0]);
+    }
+    else
+    {   
+        if(requestDetails["originUrl"])
+        {
+            return (new URL(requestDetails["originUrl"])).hostname;
+        }
+        else
+        {
+            return (new URL(requestDetails["url"])).hostname;
+        }
+    }
+}
+
+
+
+// Formats request details so that it removes uncessary details and keeping usefull data in an orginzed way to be easy accisable
 function format(requestDetails)
 {
     let formatedRecord = {};
     formatedRecord["request_size"] = requestDetails["requestSize"];
     formatedRecord["response_size"] = requestDetails["responseSize"];
-    formatedRecord["domain"] = (new URL(requestDetails["originUrl"])).hostname;
-    
-    // TODO: Think of using UTC timing instead  
-
+        
+    formatedRecord["domain"] = findOrginOfFrame(requestDetails)
+    // console.log(formatedRecord["domain"], requestDetails)
     // Formating the date so it's easily searchable
     formatedRecord["time"] = {};
     let date = new Date(requestDetails["timeStamp"]);
@@ -17,15 +41,12 @@ function format(requestDetails)
     const week_day =  date.getDay().toString();
     const day = date.getDate().toString();
     const hour = date.getHours().toString();
-    // Rounds so it only keeps every ten minuts
-    const minute = (5 * Math.round(date.getMinutes() / 5)).toString();
     
     formatedRecord["time"]["year"] = year;
     formatedRecord["time"]["month"] = year + month;
     formatedRecord["time"]["week_day"] = year + month + week_day;
     formatedRecord["time"]["day"] = year + month + week_day + day;
     formatedRecord["time"]["hour"] = year + month + week_day + day + hour;
-    formatedRecord["time"]["minute"] = year + month + week_day + day + hour + minute;
 
     return formatedRecord;
 }
@@ -46,20 +67,24 @@ request.onupgradeneeded = () =>
 {
     const db = request.result;
     const yStore = db.createObjectStore("year", {keyPath: 'id', autoIncrement: true});
-    const moStore = db.createObjectStore("month", {keyPath: 'id', autoIncrement: true});
+    const mStore = db.createObjectStore("month", {keyPath: 'id', autoIncrement: true});
     const wStore = db.createObjectStore("week_day", {keyPath: 'id', autoIncrement: true});
     const dStore = db.createObjectStore("day", {keyPath: 'id', autoIncrement: true});
     const hStore = db.createObjectStore("hour", {keyPath: 'id', autoIncrement: true});
-    const miStore = db.createObjectStore("minute", {keyPath: 'id', autoIncrement: true});
-    const stores = [yStore, moStore, wStore, dStore, hStore, miStore]
+    const stores = [yStore, mStore, wStore, dStore, hStore]
     
     stores.forEach((store, index, stores) => 
     {
+        // For checking if stored same domain and time 
         store.createIndex("domain_and_" + intervals[index], ["domain", "time." + intervals[index]], {unique: false});
+        // For getting total in a peroid
+        store.createIndex(intervals[index], "time." + intervals[index], {unique: false});
     });
     
 };
 
+
+// The process of storing/updating usage
 request.onsuccess = (event) =>
 {
 
@@ -105,3 +130,6 @@ request.onsuccess = (event) =>
         storeData, {urls: ["<all_urls>"]},["responseHeaders"]
     );
 };
+
+
+// TODO: add a way to display information
